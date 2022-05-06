b1021cedc4f7f4a2f1446102b3d09b2b46d05484
julytatiana <yulytatiana@live.com>
2022-05-06 18:27:32 -0500

    title was added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import ListOfEmployees from './components/ListOfEmployees'
 function App() {
 
   return (
+    
     <div className="main-div">
+      <h1 style={{backgroundColor: "gray", display: 'flex'}}> &nbsp;Entry Record</h1>
       <h2>Employees</h2>
       <ListOfEmployees />
       <h2>Students</h2>
